3a617fcc071f9355d5866115b26dd503290d4336
Olivia Gilbert <umhello2020@gmail.com>
2023-04-04 17:42:31 -0500

    finished models and added handlebars and homeroutes

--- models/blog_post.js
+++ models/blog_post.js
@@ -1,11 +1,31 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const User = require('./user');
 
 class Blog_Post extends Model {}
 
 Blog_Post.init(
     {
-
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        content: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: User,
+                key: 'id',
+            },
+        },
+    },
+    {
+        sequelize,
+        modelName: 'blogPost',
     }
 );
 
