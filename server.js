cdd9dc432e1b2761f37d01d45d473db5708b93ca
Olivia Gilbert <umhello2020@gmail.com>
2023-05-04 20:59:35 -0500

    updated home routes, added commentData.json and updated seed.js, updated public js folder, and added a helper.js, later to be updated

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const helpers = require('./utils/helpers')
+const hbs = exphbs.create({ helpers });
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
